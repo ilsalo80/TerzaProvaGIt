a53c6ad405a4da8c50ffbac46cca68e38254ee1f
ilsalo80 <andreasalomoni80@gmail.com>
2023-09-25 08:55:14 +0200

    fatta la modifica prevista

--- TezaProvaGit/src/Avvio.java
+++ TezaProvaGit/src/Avvio.java
@@ -4,6 +4,7 @@ public class Avvio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("avvio");
+		System.out.println("aggiungo una riga");
 	}
 
 }
